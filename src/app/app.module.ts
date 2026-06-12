a89457d6818c9885e0e5788974ef08541c62f579
gabriel.carra <gabrielcarra.p@hotmail.com>
2026-06-12 10:48:52 -0300

    Migration 17 -> 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,30 +14,22 @@ import { FooterComponent } from './modules/footer/footer.component';
 import { ConfirmationService, MessageService } from 'primeng/api';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    PageNotFoundComponent,
-    FooterComponent,
-  ],
-  imports: [
-    PrimengModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    FormsModule,
-  ],
-
-  providers: [
-    CookieService,
-    MessageService,
-    ConfirmationService,
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
-  ],
-
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        PageNotFoundComponent,
+        FooterComponent,
+    ],
+    bootstrap: [AppComponent], imports: [PrimengModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        FormsModule], providers: [
+        CookieService,
+        MessageService,
+        ConfirmationService,
+        { provide: LOCALE_ID, useValue: 'pt-BR' },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
